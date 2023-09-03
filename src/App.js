59b42abff43901cd424986560ce621f125048913
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2023-09-03 22:57:21 +0500

    added header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+/* Local Imports */
+import { Download, Features, SectionWrapper } from "./Components";
+
+import assets from "./assets/index";
+import styles from "./Styles/Global";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <SectionWrapper
+        title="Your own store of Nifty NFTs"
+        desc="Buy, Store, Collect NFTs, exchange & earn Crypto. Join Millions of People using ProNef Marketplace"
+        showBtn
+        mockupImage={assets.homeHero}
+        banner="banner"
+      >
+      </SectionWrapper>
+      <SectionWrapper
+        title="Smart User Interface Marketplace"
+        desc="Experience a buttery UI of ProNef NFTs Marketplace. Smooth constant colors of fluent UI design"
+        mockupImage={assets.homeCards}
+        reverse
+      >
+      </SectionWrapper>
+      <Features />
+      <SectionWrapper
+        title="Deployment"
+        desc="ProNef is built using Expo which runs natively on all user devices. You can easily get your app into people's hands "
+
+        mockupImage={assets.feature}
+        reverse
+      >
+      </SectionWrapper>
+      <SectionWrapper
+        title="Creative way to earn Crypto"
+        desc="This app contains two Screens. The First screen list all NFTs and the second screen is the detail screen of the NFT. "
+        mockupImage={assets.mockup}
+        banner="banner02"
+      >
+      </SectionWrapper>
+      <Download />
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>Made with love by {" "}
+          <span className="bold">JavaScript Mastery</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
